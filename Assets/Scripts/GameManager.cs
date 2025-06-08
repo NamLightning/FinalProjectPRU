35f4846e6cfd47ff4ef5a4ba437697bc0cabe3e9
NamLightning <namlightning23150@gmail.com>
2025-06-08 21:27:29 +0700

    update change Scenes Hoang to Nam

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -49,7 +49,7 @@ public class GameManager : MonoBehaviour
         score = 0;
         UpdateScore() ;
         Time.timeScale = 1;
-        SceneManager.LoadScene("Hoang");
+        SceneManager.LoadScene("Nam");
         mainMenu.SetActive(false);
         StartGame();
     }
