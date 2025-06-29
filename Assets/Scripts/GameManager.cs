ce01c84432d8c64640c44d3cd07afd2f26f5d463
Thuc Quang Le <quangthuc0602@gmail.com>
2025-06-29 16:49:12 +0700

    Fix commit error

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,197 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject gameOverUI;
+    private bool isGameOver = false;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject pauseMenu;
+
+    [SerializeField] private int maxHealth = 3;
+    private int currentHealth;
+    public int CurrentScore => score;
+
+    private PlayerController playerController;
+
+    [SerializeField]
+    private Image[] heartIcons;
+
+    [SerializeField]
+    private Sprite fullHeart;
+    [SerializeField]
+    private Sprite emptyHeart;
+    [SerializeField] private GameObject inGameUI;
+
+    void Start()
+    {
+        playerController = FindAnyObjectByType<PlayerController>();
+        Time.timeScale = 1f;
+        UpdateScore();
+        gameOverUI.SetActive(false);
+        currentHealth = maxHealth;
+
+        UpdateHearts();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void AddScore(int points)
+    {
+        if (!isGameOver)
+        {
+            score += points;
+            UpdateScore();
+        }
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+    public void GameOver()
+    {
+        if (isGameOver) return;
+
+        isGameOver = true;
+        score = 0;
+
+        playerController.PlayDeathAnimation();
+        inGameUI.SetActive(false);
+        StartCoroutine(ShowGameOverAfterDelay(1.5f));
+    }
+
+    private System.Collections.IEnumerator ShowGameOverAfterDelay(float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+
+        Time.timeScale = 0;
+        gameOverUI.SetActive(true);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isGameOver) return;
+
+        currentHealth -= damage;
+        UpdateHearts();
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+
+            GameOver();
+        }
+    }
+    public void UpdateHearts()
+    {
+        for (int i = 0; i < heartIcons.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                heartIcons[i].sprite = fullHeart;
+            }
+            else
+            {
+                heartIcons[i].sprite = emptyHeart;
+            }
+        }
+    }
+
+
+
+    //Update
+    public bool SpendScore(int amount)
+    {
+        if (isGameOver) return false;
+
+        if (score >= amount)
+        {
+            score -= amount;
+            UpdateScore();
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not enough score!");
+            return false;
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (isGameOver) return;
+
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        UpdateHearts();
+    }
+
+
+    public void RestartGame()
+    {
+        isGameOver = false;
+        score = 0;
+        currentHealth = maxHealth;
+        UpdateScore();
+        UpdateHearts();
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Hoang");
+        mainMenu.SetActive(false);
+        inGameUI.SetActive(true);
+        StartGame();
+    }
+
+
+
+    public bool IsGameOver()
+    {
+        return isGameOver;
+    }
+
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        inGameUI.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        inGameUI.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void pauseGameMenu()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+        inGameUI.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        inGameUI.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Nam");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,201 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 30f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private GameObject swordHitbox;
+    [SerializeField] private float attackDuration = 0.2f;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
+    private bool isInvincible = false;
+    [SerializeField] private float invincibilityDuration = 1f;
+    [SerializeField] private float knockbackForce = 10f;
+
+
+    //Audio 
+    AudioManager audioManager;
+
+
+    private bool isTouchingWall;
+
+
+    private Animator anim;
+    private bool isAttacking = false;
+    private bool isGrounded;
+    private bool isDead = false;
+    private Rigidbody2D rb;
+    [SerializeField]private GameManager gameManager;
+    private GameObject currentTeleporter;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gameManager = FindAnyObjectByType<GameManager>();
+        anim = GetComponent<Animator>();
+        swordHitbox.SetActive(false);
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (gameManager.IsGameOver())
+        {
+            Debug.Log("Game is over, input blocked");
+            return;
+        }
+        Debug.Log("Update running, Time.timeScale: " + Time.timeScale);
+        HandleMovement();
+        HandleJump();
+        CheckWallJump();
+        HandleAttack();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameManager.pauseGameMenu();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+    private void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        Debug.Log("Move Input: " + moveInput);
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
+        if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
+
+        anim.SetFloat("Speed", Mathf.Abs(moveInput));
+    }
+
+
+
+    private void HandleJump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        anim.SetBool("IsJumping", !isGrounded);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            audioManager.PlaySFX(audioManager.jumpSound);
+            if (isGrounded)
+            {
+               
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+            else if (!isGrounded && isTouchingWall)
+            {
+                
+                float direction = transform.localScale.x > 0 ? -1 : 1;
+                rb.linearVelocity = new Vector2(direction * moveSpeed, jumpForce);
+            }
+        }
+    }
+
+
+    private void CheckWallJump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        isTouchingWall = Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+        
+    }
+
+    private void HandleAttack()
+    {
+        if (Input.GetMouseButtonDown(0) && !isAttacking)
+        {
+            
+            StartCoroutine(PerformAttack());
+            audioManager.PlaySFX(audioManager.attackSound);
+        }
+    }
+
+    private System.Collections.IEnumerator PerformAttack()
+    {
+        isAttacking = true;
+        anim.SetTrigger("Attack");
+
+        swordHitbox.SetActive(true);
+
+        yield return new WaitForSeconds(attackDuration);
+
+        swordHitbox.SetActive(false); 
+        isAttacking = false;
+    }
+
+    // Take Hit
+    public void TakeHit(Vector2 hitSourcePosition)
+    {
+        if (isInvincible || isDead) return;
+
+
+        Vector2 knockbackDirection = (transform.position - (Vector3)hitSourcePosition).normalized;
+        knockbackDirection.y = 1f; 
+
+        rb.linearVelocity = Vector2.zero;
+        rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+
+        // anim.SetTrigger("Hurt");
+
+        StartCoroutine(InvincibilityCoroutine());
+
+        
+        gameManager.TakeDamage(1);
+    }
+    private System.Collections.IEnumerator InvincibilityCoroutine()
+    {
+        isInvincible = true;
+
+       
+        float blinkTime = 0.1f;
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        for (float i = 0; i < invincibilityDuration; i += blinkTime)
+        {
+            sr.enabled = !sr.enabled;
+            yield return new WaitForSeconds(blinkTime);
+        }
+        sr.enabled = true;
+
+        isInvincible = false;
+    }
+
+
+    public void PlayDeathAnimation()
+    {
+        if (isDead) return;
+
+        isDead = true;  
+
+        anim.SetTrigger("Death");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+}
