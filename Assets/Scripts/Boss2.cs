00a947b8226a354a3f26b97c1c9fbb1e05f5e44d
Thuc Quang Le <quangthuc0602@gmail.com>
2025-07-15 11:51:55 +0700

    "Add boss

---
+++ Assets/Scripts/Boss2.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Boss2 : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float coolDownTimer = Mathf.Infinity;
+
+    private Animator anim;
+
+
+    private void Awake()
+    { 
+    anim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        coolDownTimer += Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+            if (coolDownTimer >= attackCooldown)
+            {
+                coolDownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+
+            }
+
+        }
+
+      
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center +transform.right *range * transform.localScale.x*colliderDistance, 
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z ), 
+            0, Vector2.left, 0, playerLayer); 
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+           new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+}
